b601ffd05a42f94cefb32855c643f8f8e7069cb4
AleixMJ <aleixtechIT@gmail.com>
2026-06-18 11:16:15 +0200

    created the main hashmap file to start to create all the methods

---
+++ main.js
@@ -0,0 +1,24 @@
+
+class HashMap {
+
+    constructor() {
+        this.capacity = 16;
+        this.loadFactor = 0.75;
+        this.size = 0;
+        this.buckets = new Array(this.capacity).fill(null);
+    }
+    
+
+
+    hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+    } 
+
+}
