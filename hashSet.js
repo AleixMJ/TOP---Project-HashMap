8da48f1e62c4b01c3ef0ad6de0cb4289a2857970
AleixMJ <aleixtechIT@gmail.com>
2026-06-18 18:19:34 +0200

    completed the extra credit task hashSet.js

---
+++ hashSet.js
@@ -0,0 +1,149 @@
+class HashNode {
+    constructor(key) {
+        this.key = key;
+        this.nextNode = null;
+    }
+}
+
+class HashSet {
+
+    constructor() {
+        this.capacity = 16;
+        this.loadFactor = 0.75;
+        this.size = 0;
+        this.buckets = new Array(this.capacity).fill(null);
+    }
+    
+
+
+    hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+        hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+    } 
+
+       add(key) {
+
+        if (!this.has(key) && this.size >= this.loadFactor * this.capacity) {
+            this.increaseBuckets();
+            }
+
+        const bucketIndex = this.hash(key);
+
+        // Odin Project Guard Clause
+        if (bucketIndex < 0 || bucketIndex >= this.buckets.length) {
+            throw new Error("Trying to access index out of bound");
+        }
+
+        const newNode = new HashNode(key)
+        
+
+        if (this.buckets[bucketIndex] === null) {
+
+            this.buckets[bucketIndex] = newNode;
+            this.size++;            
+            return
+        }
+
+        let currentNode = this.buckets[bucketIndex]
+
+        while (currentNode !== null) {
+
+            if(currentNode.key === key) {                
+                return
+            }
+
+            if (currentNode.nextNode === null) {
+                currentNode.nextNode = newNode;
+                this.size++;
+                return
+            }
+            currentNode = currentNode.nextNode;
+        }
+    }
+
+    increaseBuckets() {
+        const allKeys = [...this.keys()];
+
+        this.capacity = this.capacity * 2;
+        this.buckets = new Array(this.capacity).fill(null);
+        this.size = 0;               
+        
+        for (let key of allKeys) {
+            this.add(key);        
+            }
+    }
+
+    has(key) {
+        const bucketIndex = this.hash(key);
+        let currentNode = this.buckets[bucketIndex]
+
+        while (currentNode !== null) {
+            if (currentNode.key === key) {                
+                return true;
+            }
+            currentNode = currentNode.nextNode;
+        }
+        return false;
+    }
+
+    remove(key) {
+        const bucketIndex = this.hash(key);
+        let currentNode = this.buckets[bucketIndex]
+
+        if (currentNode === null) {
+            return false;
+        }
+
+        if (currentNode.key === key) {
+            this.buckets[bucketIndex] = currentNode.nextNode;
+            this.size--;
+            return true;
+        }
+
+        let previousNode = currentNode;
+        currentNode = currentNode.nextNode;
+
+        while (currentNode !== null) {
+            if (currentNode.key === key) {
+                previousNode.nextNode = currentNode.nextNode;
+                this.size--;
+                return true;
+            }
+            previousNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+        return false;       
+    }
+
+    length() {
+        return this.size;
+    }
+
+    clear() {
+        this.capacity = 16;
+        this.buckets = new Array(this.capacity).fill(null);
+        this.size = 0;
+    }
+
+
+    keys() {
+        let result = [];
+  
+        for (let i = 0; i < this.buckets.length; i++) {
+            let currentNode = this.buckets[i];
+            while (currentNode !== null) {
+                result.push(currentNode.key);
+                currentNode = currentNode.nextNode;
+            }
+        }
+
+        return result;
+    }
+
+}
